bdf659a468d5541fa222471375cde779fa04f884
Maycon Marques <mayconmarquesh@gmail.com>
2025-04-18 19:13:13 -0300

    add authentication layout and pages for sign in and sign up

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Image from "next/image"
+
+import { Button } from "@/components/ui/button"
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+   return (
+      <main className="bg-neutral-100 min-h-screen">
+         <div className="mx-auto max-w-screen-2xl p-4">
+            <nav className="flex justify-between items-center">
+               <Image src="/logo.svg" width={152} height={56} alt="Logo" />
+               <Button variant="secondary">Sign Up</Button>
+            </nav>
+            <div className="flex flex-col items-center justify-center pt-4 md:pt-14">{children}</div>
+         </div>
+      </main>
+   )
+}
