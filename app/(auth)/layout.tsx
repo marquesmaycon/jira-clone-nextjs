5cbf08b22809677a85e14ea958002c378c93ab36
Maycon Marques <mayconmarquesh@gmail.com>
2025-04-19 16:47:29 -0300

    add class to logo image for consistent styling

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,7 +13,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       <main className="bg-neutral-100 min-h-screen">
          <div className="mx-auto max-w-screen-2xl p-4">
             <nav className="flex justify-between items-center">
-               <Image src="/logo.svg" width={152} height={56} alt="Logo" />
+               <Image src="/logo.svg" width={152} height={56} alt="Logo" className="w-[152px] h-[56px]" />
                <Button asChild variant="secondary">
                   <Link href={isSignInPage ? "/sign-up" : "/sign-in"}>{isSignInPage ? "Sign Up" : "Sign In"}</Link>
                </Button>
