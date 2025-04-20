5d7eab566acc116ee7134102cdadbb2ac25ec829
Maycon Marques <mayconmarquesh@gmail.com>
2025-04-20 19:02:39 -0300

    feat: implement workspace member management and invite code generation

--- features/workspaces/server/route.ts
+++ features/workspaces/server/route.ts
@@ -1,19 +1,38 @@
 import { zValidator } from "@hono/zod-validator"
 import { Hono } from "hono"
-import { ID } from "node-appwrite"
+import { ID, Query } from "node-appwrite"
 
+import {
+   DATABASE_ID,
+   IMAGES_BUCKET_ID,
+   MEMBERS_ID,
+   WORKSPACES_ID
+} from "@/config"
 import { sessionMiddleware } from "@/lib/session-middleware"
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config"
+import { MemberRole } from "@/features/members/types"
+import { generateInviteCode } from "@/lib/utils"
 
 import { createWorkspaceSchema } from "../schemas"
 
 const app = new Hono()
    .get("/", sessionMiddleware, async (c) => {
+      const user = c.get("user")
       const databases = c.get("databases")
+
+      const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+         Query.equal("userId", user.$id)
+      ])
+
+      if (members.total === 0) {
+         return c.json({ data: { total: 0, documents: [] } })
+      }
+
+      const workspaceIds = members.documents.map((member) => member.workspaceId)
 
       const workspaces = await databases.listDocuments(
          DATABASE_ID,
-         WORKSPACES_ID
+         WORKSPACES_ID,
+         [Query.orderDesc("$createdAt"), Query.contains("$id", workspaceIds)]
       )
 
       return c.json({ data: workspaces })
@@ -53,9 +72,16 @@ const app = new Hono()
             {
                name,
                userId: user.$id,
-               imageUrl: uploadedImageUrl
+               imageUrl: uploadedImageUrl,
+               inviteCode: generateInviteCode(7)
             }
          )
+
+         await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+            userId: user.$id,
+            workspaceId: workspace.$id,
+            role: MemberRole.ADMIN
+         })
 
          return c.json({ data: workspace })
       }
