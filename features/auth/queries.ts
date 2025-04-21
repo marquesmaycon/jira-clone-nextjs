195b0c6afa76f28863008d8cf44fd7df2247d39a
Maycon Marques <mayconmarquesh@gmail.com>
2025-04-20 22:02:43 -0300

    refactor: simplify session management by creating a session client utility

--- features/auth/queries.ts
+++ features/auth/queries.ts
@@ -1,24 +1,8 @@
-import { cookies } from "next/headers"
-import { Account, Client } from "node-appwrite"
-
-import { APPWRITE_ENDPOINT, APPWRITE_PROJECT } from "@/config"
-
-import { AUTH_COOKIE } from "./constants"
+import { createSessionClient } from "@/lib/appwrite"
 
 export const getCurrent = async () => {
    try {
-      const appCookies = await cookies()
-      const session = appCookies.get(AUTH_COOKIE)
-
-      if (!session) return null
-
-      const client = new Client()
-         .setEndpoint(APPWRITE_ENDPOINT)
-         .setProject(APPWRITE_PROJECT)
-
-      client.setSession(session.value)
-
-      const account = new Account(client)
+      const { account } = await createSessionClient()
 
       return await account.get()
    } catch {
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,33 @@
+import { cookies } from "next/headers"
+import { Account, Client, Databases } from "node-appwrite"
 import "server-only"
 
-import { Account, Client } from "node-appwrite"
 import { APPWRITE_ENDPOINT, APPWRITE_PROJECT } from "@/config"
+import { AUTH_COOKIE } from "@/features/auth/constants"
+
+export async function createSessionClient() {
+   const appCookies = await cookies()
+   const session = appCookies.get(AUTH_COOKIE)
+
+   if (!session || !session.value) {
+      throw new Error("Unauthorized")
+   }
+
+   const client = new Client()
+      .setEndpoint(APPWRITE_ENDPOINT)
+      .setProject(APPWRITE_PROJECT)
+
+   client.setSession(session.value)
+
+   return {
+      get account() {
+         return new Account(client)
+      },
+      get databases() {
+         return new Databases(client)
+      }
+   }
+}
 
 export async function createAdminClient() {
    const client = new Client()
