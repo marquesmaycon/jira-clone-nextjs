761c30e66e4d91fb30ebdc4dcce967337a62fbdf
Maycon Marques <mayconmarquesh@gmail.com>
2025-04-26 17:31:27 -0300

    fix: correct casing of setIsOpen in workspace modal hook and component

--- features/workspaces/components/create-workspace-modal.tsx
+++ features/workspaces/components/create-workspace-modal.tsx
@@ -6,10 +6,10 @@ import { CreateWorkspaceForm } from "./create-workspace-form"
 import { useCreateWorkspaceModal } from "../hooks/use-create-workspace-modal"
 
 export const CreateWorkspaceModal = () => {
-   const { isOpen, setisOpen, close } = useCreateWorkspaceModal()
+   const { isOpen, setIsOpen, close } = useCreateWorkspaceModal()
 
    return (
-      <ResponsiveModal open={isOpen} onOpenChange={setisOpen}>
+      <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
          <CreateWorkspaceForm onCancel={close} />
       </ResponsiveModal>
    )
--- features/workspaces/hooks/use-create-workspace-modal.ts
+++ features/workspaces/hooks/use-create-workspace-modal.ts
@@ -1,13 +1,13 @@
 import { parseAsBoolean, useQueryState } from "nuqs"
 
 export const useCreateWorkspaceModal = () => {
-   const [isOpen, setisOpen] = useQueryState(
+   const [isOpen, setIsOpen] = useQueryState(
       "create-workspace",
       parseAsBoolean.withDefault(false).withOptions({ clearOnDefault: true })
    )
 
-   const open = () => setisOpen(true)
-   const close = () => setisOpen(false)
+   const open = () => setIsOpen(true)
+   const close = () => setIsOpen(false)
 
-   return { isOpen, open, close, setisOpen }
+   return { isOpen, open, close, setIsOpen }
 }
