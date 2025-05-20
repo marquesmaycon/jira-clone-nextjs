ed9ef14c303aa817207beb2f3149207e1f1eb21b
Maycon Marques <mayconmarquesh@gmail.com>
2025-05-20 19:35:20 -0300

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { NuqsAdapter } from "nuqs/adapters/next/app"
-
+// comment
 import { QueryProvider } from "@/components/query-provider"
 import { Toaster } from "@/components/ui/sonner"
 import { cn } from "@/lib/utils"
